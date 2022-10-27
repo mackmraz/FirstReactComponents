a627dd73e19431c37b9144016879598503900237
Mack Mraz <mackmraz@gmail.com>
2022-10-27 14:01:13 -0400

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Nav from './components/Nav.js';
+import Promo from './components/Promo.js';
+import Intro1 from './components/Intro1.js';
+import Intro2 from './components/Intro2.js';
+import Intro3 from './components/Intro3.js';
+import Footer from './components/Footer.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Nav />
+    <Promo />
+    <Intro1 />
+    <Intro2 />
+    <Intro3 />
+    <Footer />
   </React.StrictMode>
 );
 
