f6e94651583fbb6b4717aeed5fde72bb05887b64
Mack Mraz <mackmraz@gmail.com>
2022-10-27 13:11:22 -0400

    Trying to rebase and merge branches This is a Test to Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+<<<<<<< HEAD
 import Nav from './components/Nav.js';
 import Promo from './components/Promo.js';
 import Intro1 from './components/Intro1.js';
 import Intro2 from './components/Intro2.js';
 import Intro3 from './components/Intro3.js';
 import Footer from './components/Footer.js';
+=======
+import App from './App';
+>>>>>>> 6f1d7bd (This is a Test to Initialize project using Create React App)
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+<<<<<<< HEAD
     <Nav />
     <Promo />
     <Intro1 />
     <Intro2 />
     <Intro3 />
     <Footer />
+=======
+    <App />
+>>>>>>> 6f1d7bd (This is a Test to Initialize project using Create React App)
   </React.StrictMode>
 );
 
